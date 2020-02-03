f6a490c37685542554a4b8e31f9a5e2c86a274a6
Andrey <fox451@yandex.ru>
2020-02-04 00:25:33 +0700

    add html:admin, list, detail add view

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -8,6 +14,9 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
+    objects = models.Manager()
+    published = PublishedManager()
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
@@ -24,5 +33,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug
+                       ])
 
 
