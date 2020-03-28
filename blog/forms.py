38781ca1a4d557907146345e2c19188a4c44d0b9
Andrey <fox451@yandex.ru>
2020-03-28 19:42:36 +0700

    add search posts by title, body

--- blog/forms.py
+++ blog/forms.py
@@ -14,3 +14,6 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('name', 'email', 'body')
 
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': 'admin',
+        'PASSWORD': 'superbot23',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
