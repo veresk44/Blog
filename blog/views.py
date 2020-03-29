9d00a67322b0dda4232078e43916a503f7275592
Andrey <fox451@yandex.ru>
2020-03-29 17:56:28 +0700

    add trigram search

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 
@@ -82,13 +82,20 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
+    print(request.GET)
     if 'query' in request.GET:
         form = SearchForm(request.GET)
-    if form.is_valid():
-        query = form.cleaned_data['query']
-        results = Post.objects.annotate(
-            search=SearchVector('title', 'body'),
-        ).filter(search=query)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            # results = Post.objects.annotate(
+            #     search=search_vector,
+            #     rank=SearchRank(search_vector, search_query)
+            # ).filter(rank__gte=0.3).order_by('-rank')
+            results = Post.objects.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.3).order_by('-similarity')
     return render(request, 'blog/post/search.html', {
         'form': form,
         'query': query,
